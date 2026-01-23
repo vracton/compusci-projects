2e6e68c5dad2faee505821db4d19eaed6bee4e2d
vracton <sonit.sahoo@icloud.com>
2026-01-23 10:44:22 -0600

    projectile motion level 2

--- projectile-motion/Program.cs
+++ projectile-motion/Program.cs
@@ -8,7 +8,7 @@ class ProjectileMotion
 
     static void Main(string[] args)
     {
-        LevelOne();
+        LevelTwo();
     }
 
     static void LevelOne()
@@ -22,19 +22,63 @@ class ProjectileMotion
         var v = (x: v0 * Math.Cos(theta), z: v0 * Math.Sin(theta));
         var pos = (x: 0.0, z: 0.0);
 
-        const double dt = 0.1;
+        const double dt = 0.01;
         logger.WriteLine("t", "x", "z", "v", "a");
 
-        for (int t = 0; t <= 20 * (int)(1/dt); t++)
+        for (int t = 0; t <= 20 * (int)(1 / dt); t++)
         {
             if (t > 0)
             {
                 v.z += -g * dt;
 
-                pos.x += v.x * 0.1;
-                pos.z += v.z * 0.1;
+                pos.x += v.x * dt;
+                pos.z += v.z * dt;
             }
-            logger.WriteLine($"{t * dt:F2}", $"{pos.x:F2}", $"{pos.z:F2}", $"{Math.Sqrt(v.x * v.x + v.z * v.z):F2}", -g);
+            logger.WriteLine((t * dt), pos.x, pos.z, Math.Sqrt(v.x * v.x + v.z * v.z), -g); //auto-rounds to 3 places
+        }
+    }
+
+    static void LevelTwo()
+    {
+        Logger logger = new Logger("leveltwo.txt");
+
+        const double m = 3.0;
+        const double theta = 45.0 * (Math.PI / 180.0);
+        const double v0 = 3.0;
+
+        var v = (x: v0 * Math.Cos(theta), z: v0 * Math.Sin(theta));
+        var pos = (x: 0.0, z: 0.0);
+
+        const double dt = 0.1;
+        logger.WriteLine("t", "x", "z", "v", "a");
+
+        for (int t = 0; t <= 20 * (int)(1 / dt); t++)
+        {
+            double vTotal = Math.Sqrt(v.x * v.x + v.z * v.z);
+            double dragAccel = C * vTotal * vTotal / m;
+            
+            //calculate drag angle and normalize to [0,2pi)
+            double dragTheta = Math.Atan2(v.z, v.x) + Math.PI;
+            if (dragTheta < 0)
+            {
+                dragTheta += 2 * Math.PI;
+            }
+            else if (dragTheta >= 2  * Math.PI)
+            {
+                dragTheta -= 2 * Math.PI;
+            }
+
+            var a = (x: dragAccel * Math.Cos(dragTheta), z: dragAccel * Math.Sin(dragTheta) - g); //calculate acceleration on each direction
+
+            if (t > 0)
+            {
+                v.x += a.x * dt;
+                v.z += a.z * dt;
+
+                pos.x += v.x * dt;
+                pos.z += v.z * dt;
+            }
+            logger.WriteLine(t * dt, pos.x, pos.z, Math.Sqrt(v.x * v.x + v.z * v.z), Math.Sqrt(a.x * a.x + a.z * a.z));
         }
     }
 }
