db90215b7be267bc700ae7b78ad025c868fc937e
vracton <sonit.sahoo@icloud.com>
2026-01-23 10:12:50 -0600

    logger helper class

---
+++ Logger.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+namespace Logging
+{
+    class Logger
+    {
+        private readonly string filePath;
+
+        public Logger(string fileName)
+        {
+            var projectDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", ".."));
+            var dataDir = Path.Combine(projectDir, "data");
+            Directory.CreateDirectory(dataDir);
+            var outputPath = Path.Combine(dataDir, fileName);
+
+            filePath = outputPath;
+
+            File.Create(filePath).Dispose();
+        }
+
+        public void WriteLine(params object[] vals)
+        {
+            string logToConsole = "";
+            string logToFile = "";
+
+            foreach (object o in vals)
+            {
+                logToConsole += o.ToString() + "\t";
+                logToFile += o.ToString() + " ";
+            }
+
+            logToConsole = logToConsole.Trim();
+            logToFile = logToFile.Trim() + "\n";
+
+            File.AppendAllText(filePath, logToFile);
+            Console.WriteLine(logToConsole);
+        }
+    }
+}
