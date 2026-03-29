73e86ca0f38ac0182742b109003d5f92f1264f56
vracton <vracto@icloud.com>
2026-03-28 23:01:59 -0500

    hi huber

--- Visualizer/ChemicalReactions/ChemicalDriver.cs
+++ Visualizer/ChemicalReactions/ChemicalDriver.cs
@@ -15,45 +15,55 @@ namespace Visualizer.ChemicalReactions
 {
     class ChemicalReactionsDriver
     {
+        private const double InitialHydrogenFloorM = 1.0e-7;
+        private const double Kw = 1.0e-14;
+        private const double Ka1H2So3 = 1.54881661891248e-2;
+        private const double Ka2Hso3 = 6.30957344480193e-8;
+
         static internal void Run()
         {
             const double containerSize = 25;
-
             const double deltaTime = .001;
-            const double temperature = 293.17;
-            const double reactionRadius = 5;
+            const double k1 = 650.0;
+            const double k2 = 9.2e7;
+            const double k3 = 30000.0;
+            const double chemistryRandomness = 0.03;
 
             ParticleInfo[] molecules =
             [
                 new ParticleInfo("IO_3^-", 174.903 / 6.022e23, ConvertColor(Colors.AntiqueWhite)),
                 new ParticleInfo("HSO_3^-", 81.07 / 6.022e23, ConvertColor(Colors.AntiqueWhite)),
-                new ParticleInfo("SO_4^2-", 96.06 / 6.022e23, ConvertColor(Colors.AntiqueWhite)),
-                new ParticleInfo("H^p", 1.008 / 6.022e23, ConvertColor(Colors.AntiqueWhite)),
+                new ParticleInfo("H^p", 1.008 / 6.022e23, ConvertColor(Colors.LightGoldenrodYellow)),
                 new ParticleInfo("Hg^2p", 200.59 / 6.022e23, ConvertColor(Colors.AntiqueWhite)),
-                new ParticleInfo("I^-", 126.9 / 6.022e23, ConvertColor(Colors.AntiqueWhite)),
+                new ParticleInfo("I^-", 126.9 / 6.022e23, ConvertColor(Colors.Goldenrod)),
                 new ParticleInfo("HgI_2", 454.4 / 6.022e23, ConvertColor(Colors.Coral)),
-                new ParticleInfo("I_2", 253.81 / 6.022e23, ConvertColor(Colors.AntiqueWhite)),
-                new ParticleInfo("H_2O", 18.015 / 6.022e23, ConvertColor(Colors.AntiqueWhite)),
-                new ParticleInfo("starch", 342.3 / 6.022e23, ConvertColor(Colors.AntiqueWhite)),
-                new ParticleInfo("I_2-starch", (253.81 + 342.3) / 6.022e23, ConvertColor(Colors.Blue)),
+                new ParticleInfo("I_2", 253.81 / 6.022e23, ConvertColor(Colors.MediumBlue)),
+                new ParticleInfo("H_2O", 18.015 / 6.022e23, ConvertColor(Colors.Transparent)),
             ];
-
-            (string, double)[] equations =
-            [
-                ("IO_3^-+3HSO_3^-->I^-+3SO_4^2-+3H^p", 0),
-                ("Hg^2p+2I^-->HgI_2", 0),
-                ("6H^p+IO_3^-+5I^-->3I_2+3H_2O", 0),
-                ("I_2+starch->I_2-starch", 0)
-            ];
-
-            var container = new ReactingParticleContainer(molecules, equations, containerSize, reactionRadius, 5);
-
-            var generator = new BoltzmannGenerator(container, temperature, container.Dictionary.Map["H_2O"]);
 
             var recipe = CreateDefaultRecipe();
-            SeedReactantsInLayers(container, generator, recipe);
+            var initialState = BuildInitialBulkState(recipe);
+            var container = new OldNassauParticleContainer(
+                molecules,
+                containerSize,
+                initialState,
+                [
+                    "IO_3^-",
+                    "HSO_3^-",
+                    "H^p",
+                    "Hg^2p",
+                    "I^-",
+                    "HgI_2",
+                    "I_2"
+                ],
+                tracerBudget: 1200,
+                baseK1: k1,
+                baseK2: k2,
+                baseK3: k3,
+                chemistryRandomness: chemistryRandomness);
+
             double? firstHgI2VisibleTime = null;
-            double? firstI2StarchVisibleTime = null;
+            double? firstI2VisibleTime = null;
 
             var visualization = new ChemicalVisualization(container)
             {
@@ -73,46 +83,31 @@ namespace Visualizer.ChemicalReactions
             viz.Manager.AddText("Time elapsed (s)", ConvertColor(Colors.Crimson), () => TimeElapsed().ToString());
             viz.Manager.AddText("HgI_2 molarity (M)", ConvertColor(Colors.Coral),
                 () => CalculateSpeciesMolarity(container, recipe, "HgI_2").ToString("G6"));
-            viz.Manager.AddText("I_2-starch molarity (M)", ConvertColor(Colors.Blue),
-                () => CalculateSpeciesMolarity(container, recipe, "I_2-starch").ToString("G6"));
+            viz.Manager.AddText("I_2 molarity (M)", ConvertColor(Colors.MediumBlue),
+                () => CalculateSpeciesMolarity(container, recipe, "I_2").ToString("G6"));
             viz.Manager.AddText("First time [HgI_2] >= 1e-3 (s)", ConvertColor(Colors.Coral),
                 () => FormatFirstThresholdTime(TrackFirstThresholdCrossing(container, recipe, visualization,
                     "HgI_2", 1e-3, ref firstHgI2VisibleTime)));
-            viz.Manager.AddText("First time [I_2-starch] >= 1e-5 (s)", ConvertColor(Colors.Blue),
+            viz.Manager.AddText("First time [I_2] >= 1e-5 (s)", ConvertColor(Colors.MediumBlue),
                 () => FormatFirstThresholdTime(TrackFirstThresholdCrossing(container, recipe, visualization,
-                    "I_2-starch", 1e-5, ref firstI2StarchVisibleTime)));
+                    "I_2", 1e-5, ref firstI2VisibleTime)));
 
             viz.Show();
         }
 
-        static private void AddChemicalGraphs(MotionVisualizer3DControl viz, ParticleContainer container,
+        static private void AddChemicalGraphs(MotionVisualizer3DControl viz, OldNassauParticleContainer container,
             ChemicalVisualization visualization)
         {
             var timelineInfo = new List<TimelineInfo>();
-            foreach (var info in container.Dictionary.Map.Values)
+            foreach (string speciesName in container.VisibleSpecies)
             {
+                var info = container.Dictionary.Map[speciesName];
                 var prototype = new TimelinePrototype(info.Name, info.Color);
                 timelineInfo.Add(new TimelineInfo(prototype,
-                    new BasicFunctionPair(() => visualization.Time, () => container.GetNParticles(info.Name))));
+                    new BasicFunctionPair(() => visualization.Time, () => container.GetSpeciesMolarity(info.Name))));
             }
 
-            viz.Manager.AddGraph(timelineInfo, "Time (s)", "Number of particles");
-        }
-
-        static private void AddMoleculeGraph(MotionVisualizer3DControl viz, ParticleContainer container,
-            ChemicalVisualization visualization, string title, string[] moleculeNames)
-        {
-            var timelineInfo = new List<TimelineInfo>();
-
-            foreach (string moleculeName in moleculeNames)
-            {
-                var info = container.Dictionary.Map[moleculeName];
-                var prototype = new TimelinePrototype(info.Name, info.Color);
-                timelineInfo.Add(new TimelineInfo(prototype,
-                    new BasicFunctionPair(() => visualization.Time, () => container.GetNParticles(info.Name))));
-            }
-
-            viz.Manager.AddGraph(timelineInfo, "Time (s)", title);
+            viz.Manager.AddGraph(timelineInfo, "Time (s)", "Molarity (M)");
         }
 
         private static readonly Stopwatch watch = new();
@@ -134,7 +129,7 @@ namespace Visualizer.ChemicalReactions
                 new SolutionInput(
                     "NaHSO3",
                     VolumeMl: 10,
-                    GramsPerLiter: 15.0 ,
+                    GramsPerLiter: 15.0,
                     MolarMassGPerMol: 104.06,
                     Species: [new SpeciesContribution("HSO_3^-", 1)]),
                 new SolutionInput(
@@ -142,29 +137,28 @@ namespace Visualizer.ChemicalReactions
                     VolumeMl: 10,
                     GramsPerLiter: 5.0,
                     MolarMassGPerMol: 342.3,
-                    Species: [new SpeciesContribution("starch", 1)]),
+                    Species: []),
                 new SolutionInput(
                     "HgCl2",
                     VolumeMl: 10,
-                    GramsPerLiter: 3.0,
+                    GramsPerLiter: 4.6,
                     MolarMassGPerMol: 271.49,
                     Species: [new SpeciesContribution("Hg^2p", 1)]),
                 new SolutionInput(
                     "KIO3",
                     VolumeMl: 20,
-                    GramsPerLiter: 15.0,
+                    GramsPerLiter: 11.53,
                     MolarMassGPerMol: 214.0,
                     Species: [new SpeciesContribution("IO_3^-", 1)]),
             };
 
             return new SimulationRecipe(
                 solutions,
-                ParticlesPerMolar: 16000,
+                ParticlesPerMolar: 250000,
                 MinimumParticleCount: 1);
         }
 
-        private static void SeedReactantsInLayers(ReactingParticleContainer container, RandomGenerator generator,
-            SimulationRecipe recipe)
+        private static Dictionary<string, double> BuildInitialBulkState(SimulationRecipe recipe)
         {
             double totalVolumeMl = recipe.Solutions.Sum(x => x.VolumeMl);
             if (totalVolumeMl <= 0)
@@ -172,55 +166,88 @@ namespace Visualizer.ChemicalReactions
                 throw new ArgumentException("Total solution volume must be positive.");
             }
 
-            var xRange = new DongUtility.Range(0, container.Size.X);
-            var yRange = new DongUtility.Range(0, container.Size.Y);
-            double layerStart = 0;
+            var state = new Dictionary<string, double>
+            {
+                ["IO_3^-"] = 0.0,
+                ["HSO_3^-"] = 0.0,
+                ["I^-"] = 0.0,
+                ["H^p"] = 0.0,
+                ["I_2"] = 0.0,
+                ["Hg^2p"] = 0.0,
+                ["HgI_2"] = 0.0
+            };
 
             foreach (var solution in recipe.Solutions)
             {
-                if (solution.VolumeMl <= 0)
-                {
-                    continue;
-                }
-
-                double layerFraction = solution.VolumeMl / totalVolumeMl;
-                double layerEnd = layerStart + layerFraction * container.Size.Z;
-                var zRange = new DongUtility.Range(layerStart, layerEnd);
-
+                double dilutionFactor = solution.VolumeMl / totalVolumeMl;
                 foreach (var contribution in solution.Species)
                 {
-                    int count = CalculateLayerParticleCount(solution, contribution, layerFraction, recipe);
-                    if (count > 0)
-                    {
-                        container.AddRandomParticles(generator, contribution.SpeciesName, count, xRange, yRange, zRange);
-                    }
+                    state[contribution.SpeciesName] += solution.ConcentrationM * dilutionFactor * contribution.StoichiometricFactor;
                 }
-
-                layerStart = layerEnd;
             }
+
+            state["H^p"] = EstimateInitialHydrogenFromBisulfite(state["HSO_3^-"]);
+            return state;
         }
 
-        private static int CalculateLayerParticleCount(SolutionInput solution, SpeciesContribution contribution,
-            double layerFraction, SimulationRecipe recipe)
+        private static double EstimateInitialHydrogenFromBisulfite(double totalBisulfiteMolarity)
         {
-            double scaledConcentration = solution.ConcentrationM * layerFraction * contribution.StoichiometricFactor;
-            int count = (int)Math.Round(scaledConcentration * recipe.ParticlesPerMolar);
-
-            if (count < recipe.MinimumParticleCount && scaledConcentration > 0)
+            if (totalBisulfiteMolarity <= 0)
             {
-                count = recipe.MinimumParticleCount;
+                return InitialHydrogenFloorM;
             }
 
-            return count;
+            double sodium = totalBisulfiteMolarity;
+
+            double ChargeBalance(double hydrogen)
+            {
+                double denom = hydrogen * hydrogen + Ka1H2So3 * hydrogen + Ka1H2So3 * Ka2Hso3;
+                double hso3 = totalBisulfiteMolarity * Ka1H2So3 * hydrogen / denom;
+                double so3 = totalBisulfiteMolarity * Ka1H2So3 * Ka2Hso3 / denom;
+                double hydroxide = Kw / hydrogen;
+                return (sodium + hydrogen) - (hso3 + 2.0 * so3 + hydroxide);
+            }
+
+            double low = 1.0e-9;
+            double high = 1.0;
+            double fLow = ChargeBalance(low);
+            double fHigh = ChargeBalance(high);
+
+            for (int i = 0; i < 20 && fLow * fHigh > 0; ++i)
+            {
+                low /= 10.0;
+                high *= 10.0;
+                fLow = ChargeBalance(low);
+                fHigh = ChargeBalance(high);
+            }
+
+            for (int i = 0; i < 200; ++i)
+            {
+                double mid = 0.5 * (low + high);
+                double fMid = ChargeBalance(mid);
+                if (fLow * fMid <= 0)
+                {
+                    high = mid;
+                    fHigh = fMid;
+                }
+                else
+                {
+                    low = mid;
+                    fLow = fMid;
+                }
+            }
+
+            return Math.Max(InitialHydrogenFloorM, 0.5 * (low + high));
         }
 
-        private static double CalculateSpeciesMolarity(ParticleContainer container, SimulationRecipe recipe,
+        private static double CalculateSpeciesMolarity(OldNassauParticleContainer container, SimulationRecipe recipe,
             string speciesName)
         {
-            return container.GetNParticles(speciesName) / (double)recipe.ParticlesPerMolar;
+            _ = recipe;
+            return container.GetSpeciesMolarity(speciesName);
         }
 
-        private static double? TrackFirstThresholdCrossing(ParticleContainer container, SimulationRecipe recipe,
+        private static double? TrackFirstThresholdCrossing(OldNassauParticleContainer container, SimulationRecipe recipe,
             ChemicalVisualization visualization, string speciesName, double thresholdM, ref double? firstTime)
         {
             if (firstTime is null && CalculateSpeciesMolarity(container, recipe, speciesName) >= thresholdM)
---
+++ Visualizer/ChemicalReactions/OldNassauParticleContainer.cs
@@ -0,0 +1,321 @@
+﻿using DongUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Thermodynamics;
+
+namespace Visualizer.ChemicalReactions
+{
+    internal sealed class OldNassauParticleContainer : ParticleContainer
+    {
+        private const double KspHgI2 = 2.2e-12;
+        private const double RelativeTolerance = 1.0e-4;
+        private const double MinimumChemistryStep = 1.0e-8;
+        private const double MaximumChemistryStep = 0.25;
+        private const double DefaultTracerSpeed = 0.15;
+
+        private readonly string[] visibleSpecies;
+        private readonly Dictionary<string, double> bulkState;
+        private readonly int tracerBudget;
+        private readonly Random random = new();
+        private readonly double k1;
+        private readonly double k2;
+        private readonly double k3;
+
+        public OldNassauParticleContainer(IEnumerable<ParticleInfo> particles, double side,
+            IDictionary<string, double> initialState, IEnumerable<string> visibleSpecies, int tracerBudget,
+            double baseK1, double baseK2, double baseK3, double chemistryRandomness) : base(side)
+        {
+            foreach (var particle in particles)
+            {
+                Dictionary.AddParticle(particle);
+            }
+
+            bulkState = new Dictionary<string, double>(initialState);
+            this.visibleSpecies = [..visibleSpecies];
+            this.tracerBudget = tracerBudget;
+            k1 = ApplyRandomness(baseK1, chemistryRandomness);
+            k2 = ApplyRandomness(baseK2, chemistryRandomness);
+            k3 = ApplyRandomness(baseK3, chemistryRandomness);
+
+            ParticlesToAdd.Clear();
+            ParticlesToRemove.Clear();
+            SyncTracersToBulkState();
+            ParticlesToAdd.ForEach(AddParticleDirectly);
+            ParticlesToAdd.Clear();
+            ParticlesToRemove.Clear();
+        }
+
+        public IReadOnlyList<string> VisibleSpecies => visibleSpecies;
+
+        public double Temperature
+        {
+            get
+            {
+                if (Particles.Count == 0)
+                {
+                    return 0;
+                }
+
+                double sumKineticEnergy = Particles.Sum(x => x.KineticEnergy);
+                return sumKineticEnergy / Particles.Count * 2.0 / 3.0 / Constants.BoltzmannConstant;
+            }
+        }
+
+        public double GetSpeciesMolarity(string speciesName)
+        {
+            return bulkState.GetValueOrDefault(speciesName);
+        }
+
+        public override void Update(double deltaTime)
+        {
+            ParticlesToAdd.Clear();
+            ParticlesToRemove.Clear();
+
+            foreach (var part in Particles)
+            {
+                part.Update(deltaTime);
+                CheckParticle(part);
+            }
+
+            AdvanceBulkChemistry(deltaTime);
+            SyncTracersToBulkState();
+
+            ParticlesToAdd.ForEach(AddParticleDirectly);
+            ParticlesToRemove.ForEach(RemoveParticleDirectly);
+        }
+
+        private void AdvanceBulkChemistry(double deltaTime)
+        {
+            double remaining = deltaTime;
+            double step = Math.Min(deltaTime, 0.01);
+
+            while (remaining > 0)
+            {
+                step = Math.Min(step, remaining);
+
+                var full = Rk4Step(bulkState, step);
+                var half = Rk4Step(Rk4Step(bulkState, step / 2.0), step / 2.0);
+                double error = RelativeError(full, half);
+
+                if (error > RelativeTolerance && step > MinimumChemistryStep)
+                {
+                    step = Math.Max(step / 2.0, MinimumChemistryStep);
+                    continue;
+                }
+
+                ReplaceState(half);
+                remaining -= step;
+
+                if (error < RelativeTolerance / 100.0)
+                {
+                    step = Math.Min(step * 1.5, MaximumChemistryStep);
+                }
+            }
+        }
+
+        private void SyncTracersToBulkState()
+        {
+            var targetCounts = GetTargetTracerCounts();
+            var currentBySpecies = Particles.GroupBy(x => x.Info.Name)
+                .ToDictionary(group => group.Key, group => group.ToList());
+
+            foreach (string species in visibleSpecies)
+            {
+                currentBySpecies.TryGetValue(species, out List<Molecule>? current);
+                current ??= [];
+
+                int target = targetCounts.GetValueOrDefault(species);
+                while (current.Count > target)
+                {
+                    Molecule particle = current[^1];
+                    current.RemoveAt(current.Count - 1);
+                    ParticlesToRemove.Add(particle);
+                }
+
+                while (current.Count < target)
+                {
+                    Molecule particle = Dictionary.MakeParticle(RandomPosition(), RandomVelocity(), species);
+                    ParticlesToAdd.Add(particle);
+                    current.Add(particle);
+                }
+            }
+        }
+
+        private Dictionary<string, int> GetTargetTracerCounts()
+        {
+            var tracked = visibleSpecies
+                .Where(name => name != "H^p")
+                .ToDictionary(name => name, name => Math.Max(0.0, bulkState.GetValueOrDefault(name)));
+
+            double total = tracked.Values.Sum();
+            var targets = visibleSpecies.ToDictionary(name => name, _ => 0);
+
+            if (total <= 1.0e-12)
+            {
+                return targets;
+            }
+
+            int assigned = 0;
+            foreach ((string species, double concentration) in tracked.OrderByDescending(x => x.Value))
+            {
+                int target = (int)Math.Round(tracerBudget * concentration / total);
+                if (concentration > 0 && target == 0)
+                {
+                    target = 1;
+                }
+
+                targets[species] = target;
+                assigned += target;
+            }
+
+            while (assigned > tracerBudget)
+            {
+                string species = tracked.OrderByDescending(x => targets[x.Key]).First(x => targets[x.Key] > 0).Key;
+                targets[species]--;
+                assigned--;
+            }
+
+            while (assigned < tracerBudget && tracked.Count > 0)
+            {
+                string species = tracked.OrderByDescending(x => x.Value).First().Key;
+                targets[species]++;
+                assigned++;
+            }
+
+            return targets;
+        }
+
+        private Vector RandomPosition()
+        {
+            return new Vector(
+                random.NextDouble(0, Size.X),
+                random.NextDouble(0, Size.Y),
+                random.NextDouble(0, Size.Z));
+        }
+
+        private Vector RandomVelocity()
+        {
+            return Vector.RandomDirection(DefaultTracerSpeed, random);
+        }
+
+        private void ReplaceState(Dictionary<string, double> nextState)
+        {
+            foreach ((string key, double value) in nextState)
+            {
+                bulkState[key] = value;
+            }
+        }
+
+        private Dictionary<string, double> ReactionDerivatives(Dictionary<string, double> state)
+        {
+            double io3 = state["IO_3^-"];
+            double hso3 = state["HSO_3^-"];
+            double iodide = state["I^-"];
+            double hydrogen = Math.Max(state["H^p"], 0.0);
+            double iodine = state["I_2"];
+
+            double v1 = k1 * io3 * hso3 * hydrogen;
+            double v2 = k2 * io3 * iodide * iodide * hydrogen * hydrogen;
+            double v3 = k3 * iodine * hso3;
+
+            return new Dictionary<string, double>
+            {
+                ["IO_3^-"] = -v1 - v2,
+                ["HSO_3^-"] = -3.0 * v1 - v3,
+                ["I^-"] = v1 - 5.0 * v2 + 2.0 * v3,
+                ["H^p"] = 3.0 * v1 - 6.0 * v2 + 3.0 * v3,
+                ["I_2"] = 3.0 * v2 - v3,
+                ["Hg^2p"] = 0.0,
+                ["HgI_2"] = 0.0
+            };
+        }
+
+        private static Dictionary<string, double> CombineState(Dictionary<string, double> state, double factor,
+            Dictionary<string, double> derivative)
+        {
+            var output = new Dictionary<string, double>();
+            foreach ((string key, double value) in state)
+            {
+                output[key] = Math.Max(0.0, value + factor * derivative[key]);
+            }
+
+            EnforceHgI2Solubility(output);
+            return output;
+        }
+
+        private Dictionary<string, double> Rk4Step(Dictionary<string, double> state, double dt)
+        {
+            var k1 = ReactionDerivatives(state);
+            var k2 = ReactionDerivatives(CombineState(state, dt / 2.0, k1));
+            var k3 = ReactionDerivatives(CombineState(state, dt / 2.0, k2));
+            var k4 = ReactionDerivatives(CombineState(state, dt, k3));
+
+            var output = new Dictionary<string, double>();
+            foreach ((string key, double value) in state)
+            {
+                double delta = dt * (k1[key] + 2.0 * k2[key] + 2.0 * k3[key] + k4[key]) / 6.0;
+                output[key] = Math.Max(0.0, value + delta);
+            }
+
+            EnforceHgI2Solubility(output);
+            return output;
+        }
+
+        private static double RelativeError(Dictionary<string, double> reference, Dictionary<string, double> candidate)
+        {
+            double worst = 0.0;
+            foreach ((string key, double value) in reference)
+            {
+                double scale = Math.Max(1.0e-12, Math.Max(Math.Abs(value), Math.Abs(candidate[key])));
+                worst = Math.Max(worst, Math.Abs(value - candidate[key]) / scale);
+            }
+
+            return worst;
+        }
+
+        private double ApplyRandomness(double baseline, double randomness)
+        {
+            if (randomness <= 0)
+            {
+                return baseline;
+            }
+
+            double scale = 1.0 + randomness * (2.0 * random.NextDouble() - 1.0);
+            return baseline * Math.Max(0.01, scale);
+        }
+
+        private static void EnforceHgI2Solubility(Dictionary<string, double> state)
+        {
+            double mercury = state["Hg^2p"];
+            double iodide = state["I^-"];
+
+            if (mercury <= 0.0 || iodide <= 0.0 || mercury * iodide * iodide <= KspHgI2)
+            {
+                return;
+            }
+
+            double low = 0.0;
+            double high = Math.Min(mercury, iodide / 2.0);
+
+            for (int i = 0; i < 80; ++i)
+            {
+                double x = 0.5 * (low + high);
+                double residual = (mercury - x) * Math.Pow(Math.Max(iodide - 2.0 * x, 0.0), 2);
+                if (residual > KspHgI2)
+                {
+                    low = x;
+                }
+                else
+                {
+                    high = x;
+                }
+            }
+
+            double precipitated = high;
+            state["Hg^2p"] -= precipitated;
+            state["I^-"] -= 2.0 * precipitated;
+            state["HgI_2"] += precipitated;
+        }
+    }
+}
